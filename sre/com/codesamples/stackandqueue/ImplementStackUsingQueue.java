4d54fd9b559e5ce7e9e6cca9bb8c5efd854bf200
ussun8 <hcandogan8@gmail.com>
2016-07-31 00:39:00 +0300

    Update ImplementStackUsingQueue.java

--- sre/com/codesamples/stackandqueue/ImplementStackUsingQueue.java
+++ sre/com/codesamples/stackandqueue/ImplementStackUsingQueue.java
@@ -1,1 +1,27 @@
-//TODO
+class MyStack {
+    
+    private ArrayDeque<Integer> queue1;
+    
+    public MyStack() {
+        queue1 = new ArrayDeque<>();
+    }
+    
+    public void push(int x) {
+        queue1.push(x);
+        for(int i=0; i<queue1.size()-1; i++) {
+            queue1.push(queue1.pop());
+        }
+    }
+
+    public void pop() {
+        queue1.pop();
+    }
+
+    public int top() {
+        return queue1.peek();
+    }
+
+    public boolean empty() {
+        return queue1.isEmpty();
+    }
+}
