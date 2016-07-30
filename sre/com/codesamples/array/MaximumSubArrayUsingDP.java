c83c0bb9172e2e6f266864e4996f9bf05523e7ed
ussun8 <hcandogan8@gmail.com>
2016-07-31 00:59:18 +0300

    Create MaximumSubArrayUsingDP.java

---
+++ sre/com/codesamples/array/MaximumSubArrayUsingDP.java
@@ -0,0 +1,20 @@
+import java.util.Math;
+
+public class Solution {
+    public int maxSubArray(int[] nums) {
+        
+        if(nums == null || nums.length == 0) {
+            return 0;
+        }
+        
+        int localMax = nums[0];
+        int globalMax = nums[0];
+        
+        for(int i=1; i<nums.length; i++) {
+            localMax = Math.max(localMax + nums[i], nums[i]);
+            globalMax = Math.max(globalMax, localMax);
+        }
+        
+        return globalMax;
+    }
+}
