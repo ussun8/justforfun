818f2f40941db6d5f9d705ba7ab599371f6777ed
ussun8 <hcandogan8@gmail.com>
2016-08-09 00:28:36 +0300

    Create IntegerBreak.java

---
+++ sre/com/codesamples/misc/IntegerBreak.java
@@ -0,0 +1,17 @@
+public class Solution {
+    public int integerBreak(int n) {
+        
+        if(n==2) {
+            return 1;
+        }
+        if(n==3) {
+            return 2;
+        }
+        int product = 1;
+        while(n>4) {
+            n -= 3;
+            product *= 3;
+        }
+        return product *n;
+    }
+}
