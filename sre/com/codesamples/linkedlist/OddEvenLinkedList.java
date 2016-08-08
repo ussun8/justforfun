3b5b25d12731863835a510f9b3a1db453b7f486a
ussun8 <hcandogan8@gmail.com>
2016-08-09 00:44:38 +0300

    Update OddEvenLinkedList.java

--- sre/com/codesamples/linkedlist/OddEvenLinkedList.java
+++ sre/com/codesamples/linkedlist/OddEvenLinkedList.java
@@ -1,11 +1,5 @@
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode(int x) { val = x; }
- * }
- */
+//Leetcode-328
+
 public class Solution {
     public ListNode oddEvenList(ListNode head) {
         
