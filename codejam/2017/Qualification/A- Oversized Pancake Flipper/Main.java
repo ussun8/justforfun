9185c1b191e778cb6609f399d667e4c7a93930c9
ussun8 <hcandogan8@gmail.com>
2017-06-02 10:50:25 +0300

    Update and rename codejam/2017/Qualification/PancakeFlipper.java to codejam/2017/Qualification/A- Oversized Pancake Flipper/Main.java

--- codejam/2017/Qualification/A- Oversized Pancake Flipper/Main.java
+++ codejam/2017/Qualification/A- Oversized Pancake Flipper/Main.java
@@ -2,7 +2,7 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.Scanner;
 
-public class PancakeFlipper {
+public class Main {
 
     public static void main(String[] args) throws Exception {
         Scanner in = new Scanner(new BufferedReader(new InputStreamReader(System.in)));
